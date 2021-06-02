4c5bd63ada1d53d712c0c3f1756b7147bd1d70cd
Nikola <aleksicnikola46@gmail.com>
2021-06-03 00:31:17 +0200

    Prosao kroz twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 
 
 //use http\Env\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -23,10 +24,25 @@ class ArticleController
      */
     public function show($slug){
         //return new Response('Asteroids bacon');
-        return new Response(sprintf(
-            'Asteroids slug: %s',
-            $slug
-        ));
+//        return new Response(sprintf(
+//            'Asteroids slug: %s',
+//            $slug
+//        ));
+//        return $this->render('article/show.html.twig', [
+//            'title' => ucwords(str_replace('-', ' ', $slug)),
+//        ]);
+
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 
+    //PITANJA:
+    //STA RADI fja ucwords() i ostale njoj slicne
 }
