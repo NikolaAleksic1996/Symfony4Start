87595572dd10be2b682a4800c448effea5001377
Nikola <aleksicnikola46@gmail.com>
2021-06-03 17:06:16 +0200

    Kreiranje servisa

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,8 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
 ];
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,44 +5,75 @@ namespace App\Controller;
 
 
 //use http\Env\Response;
+use App\Service\Markdownhelper;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage(): Response
     {
-        return new Response('Go!');
+        return $this->render('article/homepage.html.twig', [
+            'title' => '',
+            'comments' => [],
+        ]);
+        //dodavanje stranice
+        //return new Response('Go!');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/article/{slug}", name="article_show")
      */
-    public function show($slug){
-        //return new Response('Asteroids bacon');
-//        return new Response(sprintf(
-//            'Asteroids slug: %s',
-//            $slug
-//        ));
-//        return $this->render('article/show.html.twig', [
-//            'title' => ucwords(str_replace('-', ' ', $slug)),
-//        ]);
+    public function showAction($slug, Markdownhelper $markdownhelper)
+    {
+
 
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
+
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non **cupim**.
+EOF;
+        //$articleContent = $markdown->transform($articleContent);
+        ////markamo i oznacavamo tekst
+
+        //dump($cache);die;
+        //ovako nije dobro jer nemoze da se testira pa pravimo posebnu klasu sopstveni servis
+       $articleContent = $markdownhelper->parse($articleContent);//ovako pvamo service
+
+
+
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
+            'articleContent' => $articleContent,
             'comments' => $comments,
-        ]);
+        ]);//prvo slovo veliko i '-' ako pronadje sa ' ' menja
     }
 
-    //PITANJA:
-    //STA RADI fja ucwords() i ostale njoj slicne
+    /**
+     * @Route("/article/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+
+        $logger->info('Article is being hearted');
+
+        //return new Response(json_encode(['hearts' => 5]));
+        return new JsonResponse(['heart' => rand(5, 100)]);
+        // return $this->json(['heart' =>rand(5,100)]);
+    }
+
 }
